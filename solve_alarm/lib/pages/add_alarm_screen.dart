f4cb71fe4bed697671ee9533afc687dca5c9955f
NicM28 <nicola.muellener@bluewin.ch>
2024-07-15 00:29:01 +0200

    added code to main.dart

---
+++ solve_alarm/lib/pages/add_alarm_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddAlarmScreen extends StatefulWidget {
+  const AddAlarmScreen({super.key});
+
+  @override
+  State<AddAlarmScreen> createState() => _AddAlarmScreenState();
+}
+
+class _AddAlarmScreenState extends State<AddAlarmScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
