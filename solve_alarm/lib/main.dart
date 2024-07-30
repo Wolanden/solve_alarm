ab5928fff6b10097b5143b3ffa02354737b6965b
NicM28 <nicola.muellener@bluewin.ch>
2024-07-30 16:32:01 +0200

    added alarm ringing

--- solve_alarm/lib/main.dart
+++ solve_alarm/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:solve_alarm/pages/add_alarm_screen.dart';
 import 'package:solve_alarm/pages/edit_alarm_screen.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
+// Main screen of the alarm app
 class AlarmScreen extends StatefulWidget {
   const AlarmScreen({super.key});
 
@@ -31,28 +32,101 @@ class AlarmScreen extends StatefulWidget {
 }
 
 class _AlarmScreenState extends State<AlarmScreen> {
-  List<Map<String, dynamic>> alarms = [];
-  final AudioPlayer _audioPlayer = AudioPlayer();
+  List<Map<String, dynamic>> alarms = []; // List to store all alarms
+  final AudioPlayer _audioPlayer = AudioPlayer(); // Audio player for alarm sounds
+  Timer? _timer; // Timer to periodically check alarms
+  Map<String, dynamic>? _currentlyRingingAlarm; // Currently active alarm
+
+  @override
+  void initState() {
+    super.initState();
+    _startAlarmChecker();
+  }
 
   @override
   void dispose() {
+    _timer?.cancel();
     _audioPlayer.dispose();
     super.dispose();
   }
+  // Start a periodic timer to check alarms
+  void _startAlarmChecker() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      _checkAlarms();
+    });
+  }
+  // Check if any alarms should ring
+  void _checkAlarms() {
+    if (_currentlyRingingAlarm != null) {
+      // An alarm is already ringing, don't check for new alarms
+      return;
+    }
 
+    final now = DateTime.now();
+    final currentTime = '${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}';
+    final currentDay = now.weekday - 1; // 0 for Monday, 6 for Sunday
+
+    for (var alarm in alarms) {
+      if (alarm['isActive'] && 
+          alarm['time'] == currentTime && 
+          alarm['days'][currentDay]) {
+        _ringAlarm(alarm);
+        break;
+      }
+    }
+  }
+  // Trigger the alarm
+  void _ringAlarm(Map<String, dynamic> alarm) {
+    setState(() {
+      _currentlyRingingAlarm = alarm;
+    });
+    _audioPlayer.play(AssetSource('sounds/${alarm['sound']}'));
+    _audioPlayer.setReleaseMode(ReleaseMode.loop);
+    _showAlarmDialog(alarm);
+  }
+  // Show a dialog when an alarm is ringing
+  void _showAlarmDialog(Map<String, dynamic> alarm) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Alarm!'),
+          content: Text('Zeit: ${alarm['time']}'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Stoppen'),
+              onPressed: () {
+                _stopAlarm();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+  // Stop the currently ringing alarm
+  void _stopAlarm() {
+    _audioPlayer.stop();
+    setState(() {
+      _currentlyRingingAlarm = null;
+    });
+  }
+  // Add a new alarm to the list
   void _addAlarm(Map<String, dynamic> newAlarm) {
     setState(() {
       alarms.add({...newAlarm, 'isActive': true});
     });
   }
-
+  // Toggle an alarm's active state
   void _toggleAlarmActive(int index, bool isActive) {
     setState(() {
       alarms[index]['isActive'] = isActive;
     });
   }
 
-  @override
+    @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -65,66 +139,93 @@ class _AlarmScreenState extends State<AlarmScreen> {
         itemBuilder: (context, index) {
           final alarm = alarms[index];
           return Container(
+            // ... (UI code for each alarm item)
             margin: const EdgeInsets.all(20),
             padding: const EdgeInsets.all(10),
             decoration: BoxDecoration(
               color: const Color.fromARGB(255, 112, 112, 112),
               borderRadius: BorderRadius.circular(10),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Column(
               children: [
-                Text(alarm['time'],
-                    style: const TextStyle(color: Colors.white, fontSize: 30)),
-                const Spacer(),
-                Switch(
-                  value: alarm['isActive'],
-                  onChanged: (value) {
-                    _toggleAlarmActive(index, value);
-                  },
-                  activeColor: Colors.blue,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(alarm['time'],
+                        style: const TextStyle(color: Colors.white, fontSize: 30)),
+                    const Spacer(),
+                    Switch(
+                      value: alarm['isActive'],
+                      onChanged: (value) {
+                        _toggleAlarmActive(index, value);
+                      },
+                      activeColor: Colors.blue,
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        if (alarm['sound'] != null && alarm['sound'].isNotEmpty) {
+                          await _audioPlayer
+                              .play(AssetSource('sounds/${alarm['sound']}'));
+                        }
+                      },
+                      icon: const Icon(Icons.play_arrow),
+                      color: Colors.white,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const EditAlarmScreen()));
+                        },
+                        color: Colors.white,
+                        icon: const Icon(Icons.settings),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            alarms.removeAt(index);
+                          });
+                        },
+                        icon: const Icon(Icons.delete),
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
                 ),
-                IconButton(
-                  onPressed: () async {
-                    if (alarm['sound'] != null && alarm['sound'].isNotEmpty) {
-                      await _audioPlayer
-                          .play(AssetSource('sounds/${alarm['sound']}'));
-                    }
-                  },
-                  icon: const Icon(Icons.play_arrow),
-                  color: Colors.white,
-                ),
-                Container(
-                  margin: const EdgeInsets.only(right: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const EditAlarmScreen()));
-                    },
-                    color: Colors.white,
-                    icon: const Icon(Icons.settings),
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        alarms.removeAt(index);
-                      });
-                    },
-                    icon: const Icon(Icons.delete),
-                    color: Colors.white,
-                  ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    for (int i = 0; i < 7; i++)
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 2),
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          color: alarm['days'][i] ? Colors.blue : Colors.grey,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: Text(
+                          ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'][i],
+                          style: TextStyle(
+                            color: alarm['days'][i] ? Colors.white : Colors.black,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                  ],
                 ),
               ],
             ),
